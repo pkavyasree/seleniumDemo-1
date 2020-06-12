9adfd42038eca4c9ebc1e070fadaf46fc3727673
majid2709 <63237287+majid2709@users.noreply.github.com>
2020-06-12 12:20:48 +0530

    updated

--- src/test/java/com/test/Demo.java
+++ src/test/java/com/test/Demo.java
@@ -17,7 +17,7 @@ public class Demo {
 	public void driverInit() {
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//chromedriver");
 		ChromeOptions options = new ChromeOptions();
-		options.setExperimentalOption("prefs", chromePrefs);
+		//options.setExperimentalOption("prefs", chromePrefs);
                 options.addArguments("--no-sandbox");
                 options.addArguments("--headless");
                 options.addArguments("--disable-dev-shm-usage");
